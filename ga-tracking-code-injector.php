0f1c1c56f6feee3b96a2e9d958b26eec76f26742
alextryonpdx <alextryonpdx@gmail.com>
2018-03-20 16:09:50 -0700

    structure

--- ga-tracking-code-injector.php
+++ ga-tracking-code-injector.php
@@ -1,4 +1,15 @@
 <?php
+ /*
+ Plugin Name: GA Tracking Code Injector (WC)
+ Plugin URI: https://github.com/Watson-Creative/GA-Tracking-Code-Injector
+ GitHub Plugin URI: https://github.com/Watson-Creative/GA-Tracking-Code-Injector
+ description: Inject GA code in appropriate places site-wide - use custom menu to set tracking code ID
+ Version: 0.0.1
+ Author: Alex Tryon
+ Author URI: http://www.alextryonpdx.com
+ License: GPL2
+ */
+
 
 function printGAcode(){
 	echo '<div id="PANTHEONVARDUMP">';
