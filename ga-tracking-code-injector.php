6d5b301f856016788af2a647a608719b3acd6399
alextryonpdx <alextryonpdx@gmail.com>
2018-03-20 16:47:56 -0700

    got it working... option page works, saves data, outputs it as expected... need to test the GA code and make sure it fires (doesnt look like scrypt is loading properly

--- ga-tracking-code-injector.php
+++ ga-tracking-code-injector.php
@@ -4,36 +4,53 @@
  Plugin URI: https://github.com/Watson-Creative/GA-Tracking-Code-Injector
  GitHub Plugin URI: https://github.com/Watson-Creative/GA-Tracking-Code-Injector
  description: Inject GA code in appropriate places site-wide - use custom menu to set tracking code ID
- Version: 0.0.1
+ Version: 0.0.2
  Author: Alex Tryon
  Author URI: http://www.alextryonpdx.com
  License: GPL2
  */
 
 
+if ( is_admin() ){ // admin actions
+  add_action( 'admin_menu', 'ga_inject_create_menu' );
+  add_action( 'admin_init', 'register_ga_inject_settings' );
+}
+
+
+
 function printGAcode(){
-	echo '<h1>' . get_option('ga_inject_code') . '</h1>';
+	//echo '<h1 id="testBlock">' . get_option('ga_inject_code') . '</h1>'; //debug message
 	// echo '<div id="PANTHEONVARDUMP">';
 	// $ENV = $_ENV['PANTHEON_ENVIRONMENT'];
 	// if( $ENV == 'live') {
-	// 	echo '<script async src="https://www.googletagmanager.com/gtag/js?id=UA-116061038-1"></script>
-	// 	<script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag("js", new Date());gtag("config", "UA-116061038-1");</script>';
+		echo '<script async src="https://www.googletagmanager.com/gtag/js?id=' . get_option("ga_inject_code") . '"></script>
+		<script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag("js", new Date());gtag("config", "' . get_option("ga_inject_code") . '");</script>';
 	// }
 
 	// echo '</div>';
 }
-add_action('wp_header', 'printGAcode');
-// add_action('wp_head', 'printGAcode');
+// add_action('wp_headers', 'printGAcode');
+add_action('wp_head', 'printGAcode');
+
+
+
+
+register_activation_hook(__FILE__,'create_default_values');
+function create_default_values() {
+if ( get_option( 'ga_inject_code' ) == false ) { 
+		add_option("ga_inject_code", 'UA-XXXXX-X'); 
+	}
+}
 
 
 function register_ga_inject_settings() { // whitelist options
-	register_setting( 'ga_inject_option-group', 'ga_inject_code' );
+	register_setting( 'ga-inject-option-group', 'ga_inject_code' );
 }
 
-function freeze_create_menu() {
+function ga_inject_create_menu() {
 
 	//create new top-level menu
-	add_menu_page('GA Code Injector Settings', 'GA Inject Settings', 'administrator', __FILE__, 'ga_inject_settings_page', plugins_url('img/WC_Brand-20.png', __FILE__ ) );
+	add_menu_page('GA Code Injector Settings', 'GA Code Injector Settings', 'administrator', __FILE__, 'ga_inject_settings_page', plugins_url('img/WC_Brand-20.png', __FILE__ ) );
 
 	//call register settings function
 	add_action( 'admin_init', 'register_ga_inject_settings' );
@@ -47,13 +64,13 @@ function ga_inject_settings_page() {
 	<h1>Watson Creative Google Analytics Code Injector</h1>
 	<form method="post" action="options.php"> 
 		<?php 
-		settings_fields( 'ga_inject_code-group' );
-		do_settings_sections( 'ga_inject_code-group' ); ?>
+		settings_fields( 'ga-inject-option-group' );
+		do_settings_sections( 'ga-inject-option-group' ); ?>
 
-		<table class="form-table ga_inject_code-options">
+		<table class="form-table ga-inject-code-options">
 
 	        <tr valign="top">
-		        <th scope="row">Tracking Code (AZ-1234567-8)</th>
+		        <th scope="row">Tracking Code (UA-XXXXX-X)</th>
 		        <td><input type="text" name="ga_inject_code" value="<?php echo esc_attr( get_option('ga_inject_code') ); ?>" /></td>
 	        </tr>
 	  
