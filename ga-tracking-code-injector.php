1417b49d35c812610a9cb5f9eae25d93e3c9237e
alextryonpdx <alextryonpdx@gmail.com>
2018-07-27 14:19:55 -0700

    added facbook tracking pixel code option

--- ga-tracking-code-injector.php
+++ ga-tracking-code-injector.php
@@ -3,8 +3,8 @@
  Plugin Name: GA Tracking Code Injector (WC)
  Plugin URI: https://github.com/Watson-Creative/GA-Tracking-Code-Injector
  GitHub Plugin URI: https://github.com/Watson-Creative/GA-Tracking-Code-Injector
- description: Add tags for Google Analytics and Google Tag Manager in appropriate locations globally from WP Admin menu. Code is only printed in a live Pantheon environment to prevent skewing data with traffic on the development or testing environments.
- Version: 2.0.1
+ description: Add tags for Google Analytics, Google Tag Manager and Facebook Pixel(since 2.1.0) code in appropriate locations globally from WP Admin menu. Code is only printed in a live Pantheon environment to prevent skewing data with traffic on the development or testing environments.
+ Version: 2.1.0
  Author: Alex Tryon
  Author URI: http://www.alextryonpdx.com
  License: GPL2
@@ -38,6 +38,28 @@ function printGAcode_head(){
 				'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 				})(window,document,'script','dataLayer','" . $GTM_CODE . "');</script>
 				<!-- End Google Tag Manager -->";
+		endif;
+
+		// Google Tag Manager Header Code
+		$FB_PIXEL_CODE = get_option("fb_pixel_code");
+		if($FB_PIXEL_CODE != '###############' && $FB_PIXEL_CODE != ''):
+			echo '<!-- Facebook Pixel Code -->
+				<script>
+				!function(f,b,e,v,n,t,s)
+				{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+				n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+				if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version=\'2.0\';
+				n.queue=[];t=b.createElement(e);t.async=!0;
+				t.src=v;s=b.getElementsByTagName(e)[0];
+				s.parentNode.insertBefore(t,s)}(window, document,\'script\',
+				\'https://connect.facebook.net/en_US/fbevents.js\');
+				fbq(\'init\', \'' . $FB_PIXEL_CODE . '\');
+				fbq(\'track\', \'PageView\');
+				</script>
+				<noscript><img height="1" width="1" style="display:none"
+				src="https://www.facebook.com/tr?id=' . $FB_PIXEL_CODE . '&ev=PageView&noscript=1"
+				/></noscript>
+				<!-- End Facebook Pixel Code -->';
 		endif;
 	}
 }
@@ -80,12 +102,16 @@ function create_default_values() {
 	if ( get_option( 'gtm_inject_code' ) == false ) { 
 		add_option("gtm_inject_code", 'GTM-XXXX'); 
 	}
+	if ( get_option( 'fb_pixel_code' ) == false ) { 
+		add_option("fb_pixel_code", '###############'); 
+	}
 }
 
 
 function register_ga_inject_settings() { // whitelist options
 	register_setting( 'ga-inject-option-group', 'ga_inject_code' );
 	register_setting( 'ga-inject-option-group', 'gtm_inject_code' );
+	register_setting( 'ga-inject-option-group', 'fb_pixel_code' );
 }
 
 function ga_inject_create_menu() {
@@ -119,6 +145,11 @@ function ga_inject_settings_page() {
 	        <tr valign="top">
 		        <th scope="row">Google Tag Manager Container ID (GTM-XXXX)</th>
 		        <td><input type="text" name="gtm_inject_code" value="<?php echo esc_attr( get_option('gtm_inject_code') ); ?>" /></td>
+	        </tr>
+
+	        <tr valign="top">
+		        <th scope="row">Facebook Pixel Code (###############)</th>
+		        <td><input type="text" name="fb_pixel_code" value="<?php echo esc_attr( get_option('fb_pixel_code') ); ?>" /></td>
 	        </tr>
 	  
 	    </table>
